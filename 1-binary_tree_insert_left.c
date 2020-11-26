4a584682bc4811c50ba76521bea4d79bc0fa47a7
didierrevelo <1829@holbertonschool.com>
2020-11-26 02:34:48 -0500

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		new_son->left = parent->left;
 		parent->left->parent = new_son;
-		parent->left = new_son;
 	}
 	parent->left = new_son;
 	return (new_son);
