36057e1285d9ef05ac3d9acf4e3f77955332f6a0
didierrevelo <1829@holbertonschool.com>
2020-11-27 12:52:44 -0500

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,13 +6,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    int depth = 0;
-    if (tree == NULL)
-        return (0);
-    while (tree->parent != NULL)
-    {
-        depth++;
-        tree = tree->parent;
-    }
-    return (depth);
+	int depth = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
