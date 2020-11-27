22f4f05520610ef0e332ff0f062d3e3010012669
didierrevelo <1829@holbertonschool.com>
2020-11-26 21:04:37 -0500

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 /**
+* binary_tree_delete -  deletes an entire binary tree
+* @tree: is a pointer to the root node of the tree to delete
+* Return: nothing
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
